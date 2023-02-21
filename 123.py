81c08aa5aad1d8ae2614a1a4e63629587b76df95
Margarita <margodevalois@gmail.com>
2023-02-21 12:10:25 +0100

    Add description

--- 123.py
+++ 123.py
@@ -1,11 +1,13 @@
 # Introducing Python. Modern Computing in Simple Packages.Bill Lubanovic
-# INTRODUCTION
 
-# Ex.1-3 - print list with conditions
+# PY FILLING
+
+# # Ex.1-3 - print list with conditions
 # years_list = [1993, 1994, 1995, 1996, 1997, 1998]
 # print(years_list[3])
 # print(max(years_list))
-# # 4-7
+
+# # Ex.4-7 - change list
 # things = ['mozzarella', 'cinderella', 'salmonella']
 # things[1] = things[1].title()
 # print(things)
@@ -13,13 +15,15 @@
 # print(things)
 # things.pop(2)
 # print(things)
-# # 8,9
+
+# # Ex.8,9 - change list
 # suprise = ['Groucho', 'Chico', 'Harpo']
 # suprise[-1] = suprise[-1].lower()[::-1].capitalize()
 # #suprise[-1] = suprise[-1][::-1]
 # #suprise[-1] = suprise[-1].capitalize()
 # print(suprise)
-# # 10-14
+
+# # Ex.10-14 - deals with dict
 # e2f = {'dog': 'chien', 'cat': 'chat', 'walrus': 'morse'}
 # print(e2f)
 # print(e2f['walrus'])
@@ -28,7 +32,8 @@
 # keys = [k for k, v in f2e.items() if v == 'chien']
 # print(keys)
 # print(set(e2f))
-# # 15-18
+
+# # Ex.15-18 - multilayers dict
 # life = {'animals': {'cats': ['Henri', 'Grumpy', 'Lucy'],
 #                     'octopi': {},
 #                     'emus': {}},
@@ -38,7 +43,10 @@
 # print(life['animals'].keys())
 # print(life['animals']['cats'])
 
-# 1,2
+
+# PY CRUST
+
+# # Ex.1,2 - if, else, elif
 # guess_me = 7
 # start = 1
 # # if guess_me < 7:
@@ -56,40 +64,39 @@
 #     start += 1
 # else:
 #     if start > guess_me:
-#         print('oops!')
-#
-# # 3 цикл for
+#         print
+
+# # Ex.3 - for
 # l1 = [3, 2, 1, 0]
 # for i in l1:
 #     print(i)
-#
-# # 4 включение списка
+
+# # Ex.4 - list comprehensions
 # l2 = [i for i in range(10) if i % 2 == 1]
 # print(l2)
-#
-# # 5 включение словаря
+
+# # Ex.5 - dict comprehensions
 # # s = range(10)
 # squares = {s: pow(s, 2) for s in range(10)}
 # print(squares)
-#
-# # 6 включение множества
+
+# # Ex.6 - set comprehensions
 # odd = {i for i in range(10) if i % 2 == 0}
 # print(odd)
-#
-# # 7 включение генератора
-#
+
+# # Ex.7 - generator comprehensions
 # generator1 = [i for i in range(10)]
 # for x in generator1:
 #     print(x)
 # print('Got', len(generator1))
-#
-# # 8
+
+# # Ex.8
 # def good():
 #     list = ['Harry', 'Ron', 'Hermione']
 #     return list
 # print(good())
-#
-# # 9
+
+# # Ex.9 - generators
 # def get_odds():
 #     output = []
 #     for x in range(1, 10):
@@ -99,8 +106,8 @@
 # for i in get_odds():
 #     if i == get_odds()[2]:
 #         print(i)
-#
-# # 10 определение декоратора
+
+# # Ex.10 - decorator
 # def test(func):
 #     def wrapper():
 #         print('start of function func()')
@@ -112,41 +119,27 @@
 # def test1():
 #     print('lll')
 # test1()
-#
-# # 11 определение исключения
-# # class OopsException(Exception):
-# #     pass
-# #
-# #
-# # words = ['lll', 'mmm', 'nnn']
-# # for w in words:
-# #     if w == 'nnn':
-# #         raise OopsException(w)
-# # try:
-# #     raise OopsException('oooo')
-# # except OopsException as exc:
-# #     print(exc)
-#
-# # 12 с помощью zip создаем словрь из списков
+
+# # Ex.12 - dict from lists by zip
 # titles = ['Creature of habit', 'Crewel Fate']
 # plots = ['A nun turns into a monster', 'A haunted yarn shop']
 # print(dict(zip(titles, plots)))
 
-# class 1
+#  Objects and Classes
+# # Ex.1
 # class Thing:
 #     pass
 #
 # example = Thing()
 # print(Thing)
 # print(example)
-#
-# # class 2
+
+# # Ex.2
 # class Thing2:
 #    letters = 'abc'
-#
 # print(Thing2.letters)
-#
-# # class 3
+
+# # Ex.3
 # class Thing3:
 #     def __init__(self, letters):
 #         self.letters = letters
@@ -155,7 +148,7 @@
 # smth.letters = "xyz"
 # print(smth.letters)
 
-# class 4, 5, 6, 7
+# # Ex. 4-7
 # class Element:
 #     def __init__(self, name, symbol, number):
 #         self.name = name
@@ -178,65 +171,65 @@
 # hydrogen = Element(**D)
 # hydrogen.dump()
 
-# 8
-class Element:
-    def __init__(self, name, symbol, number):
-        self.__name = name
-        self.__symbol = symbol
-        self.__number = number
-    @property
-    def name(self):
-        return self.__name
-    @property
-    def ymbol(self):
-        return self.__symbol
-    @property
-    def number(self):
-        return self.__number
+# # Ex.8 - modyfing Element
+# class Element:
+#     def __init__(self, name, symbol, number):
+#         self.__name = name
+#         self.__symbol = symbol
+#         self.__number = number
+#     @property
+#     def name(self):
+#         return self.__name
+#     @property
+#     def ymbol(self):
+#         return self.__symbol
+#     @property
+#     def number(self):
+#         return self.__number
+#
+#
+# hydrogen = Element('Hydrogen', 'H', 1)
+# print(hydrogen.name)
 
+# # Ex.9
+# class Bear:
+#     def eats(self):
+#         return 'berries'
+# class Rabbit:
+#     def eats(self):
+#         return 'clover'
+# class Octothorpe:
+#     def eats(self):
+#         return 'campers'
+#
+# B = Bear()
+# R = Rabbit()
+# O = Octothorpe()
+#
+# print(B.eats())
+# print(R.eats())
+# print(O.eats())
 
-hydrogen = Element('Hydrogen', 'H', 1)
-print(hydrogen.name)
-
-# 9
-class Bear:
-    def eats(self):
-        return 'berries'
-class Rabbit:
-    def eats(self):
-        return 'clover'
-class Octothorpe:
-    def eats(self):
-        return 'campers'
-
-B = Bear()
-R = Rabbit()
-O = Octothorpe()
-
-print(B.eats())
-print(R.eats())
-print(O.eats())
-
-# 10
-class Laser:
-    def does(self):
-        return 'disintegrate'
-
-class Claw:
-    def does(self):
-        return 'crush'
-
-class SmartPhone:
-    def does(self):
-        return 'ring'
-
-class Robot:
-    def __init__(self):
-        self.laser = Laser()
-        self.claw = Claw()
-        self.smartphone = SmartPhone()
-    def does(self):
-        print(self.laser.does(), self.claw.does(), self.smartphone.does())
-
-R = Robot()
-print(R.does())
+# # Ex.10
+# class Laser:
+#     def does(self):
+#         return 'disintegrate'
+#
+# class Claw:
+#     def does(self):
+#         return 'crush'
+#
+# class SmartPhone:
+#     def does(self):
+#         return 'ring'
+#
+# class Robot:
+#     def __init__(self):
+#         self.laser = Laser()
+#         self.claw = Claw()
+#         self.smartphone = SmartPhone()
+#     def does(self):
+#         print(self.laser.does(), self.claw.does(), self.smartphone.does())
+#
+# R = Robot()
+# print(R.does())
